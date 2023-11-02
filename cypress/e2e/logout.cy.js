f56415562ff1ec2e78b4dbc9a542aa3bc3fcb1d6
mathiasg12 <mathiasgausl@gmail.com>
2023-11-02 15:14:33 +0100

    added the last e2e test that tests if a user can log out with the log out buttin

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,16 @@
+describe("test that users can log out with the log out button", () => {
+  it("users can log in and then press the logout button to log out", () => {
+    cy.visit("index.html").wait(8000);
+    cy.get("#registerForm > .modal-footer > .btn-outline-success")
+      .click()
+      .wait(1500);
+    cy.get("#loginEmail").type("test12345@noroff.no", { delay: 100 });
+    cy.get("#loginPassword").type("abc12345", { delay: 100 });
+    cy.get("#loginForm > .modal-footer > .btn-success").click();
+    cy.intercept(
+      "https://nf-api.onrender.com/api/v1/social/profiles/JS2TEST2?&_followers=true&_posts=true&_following=true",
+    ).as("apiResp");
+    cy.wait("@apiResp");
+    cy.get(".btn-outline-warning").click();
+  });
+});
