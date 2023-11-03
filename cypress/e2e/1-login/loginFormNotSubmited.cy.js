e75e7683f54d7304ca1a2b5ed8270897117bb9fa
mathiasg12 <mathiasgausl@gmail.com>
2023-11-03 12:15:55 +0100

    fixed small issues with some e2e test

--- cypress/e2e/1-login/loginFormNotSubmited.cy.js
+++ cypress/e2e/1-login/loginFormNotSubmited.cy.js
@@ -9,10 +9,16 @@ describe("users can not submit the login form with invalid credentials and is sh
     cy.get("#loginForm > .modal-footer > .btn-success").click();
     cy.get("#loginForm > .modal-body").within(() => {
       cy.get("input:invalid").should("have.length", 1);
+      cy.get("input:invalid").should("be.visible");
+      cy.get("input:invalid").should(
+        "have.attr",
+        "title",
+        "Only Noroff student or teacher emails are valid.",
+      );
     });
   });
   it("stops the form from being submitted when given no email and shows a message", () => {
-    cy.visit("index.html").wait(2000);
+    cy.visit("index.html").wait(8000);
     cy.get("#registerForm > .modal-footer > .btn-outline-success")
       .click()
       .wait(1500);
@@ -20,10 +26,11 @@ describe("users can not submit the login form with invalid credentials and is sh
     cy.get("#loginForm > .modal-footer > .btn-success").click();
     cy.get("#loginForm > .modal-body").within(() => {
       cy.get("input:invalid").should("have.length", 1);
+      cy.get("input:invalid").should("be.visible");
     });
   });
   it("stops the form from being submitted when given no password and shows a message", () => {
-    cy.visit("index.html").wait(2000);
+    cy.visit("index.html").wait(8000);
     cy.get("#registerForm > .modal-footer > .btn-outline-success")
       .click()
       .wait(1500);
@@ -31,6 +38,7 @@ describe("users can not submit the login form with invalid credentials and is sh
     cy.get("#loginForm > .modal-footer > .btn-success").click();
     cy.get("#loginForm > .modal-body").within(() => {
       cy.get("input:invalid").should("have.length", 1);
+      cy.get("input:invalid").should("be.visible");
     });
   });
 });
--- cypress/e2e/1-login/logout.cy.js
+++ cypress/e2e/1-login/logout.cy.js
@@ -1,6 +1,6 @@
 describe("test that users can log out with the log out button", () => {
   it("users can log in and then press the logout button to log out", () => {
-    cy.visit("index.html").wait(8000);
+    cy.visit("index.html").wait(1000);
     cy.get("#registerForm > .modal-footer > .btn-outline-success")
       .click()
       .wait(1500);
@@ -11,8 +11,10 @@ describe("test that users can log out with the log out button", () => {
       "https://nf-api.onrender.com/api/v1/social/profiles/JS2TEST2?&_followers=true&_posts=true&_following=true",
     ).as("apiResp");
     cy.wait("@apiResp");
+    cy.window().its("localStorage.token").should("exist");
     cy.get(".btn-outline-warning").click();
     cy.wait(3000);
     cy.window().its("localStorage.token").should("not.exist");
+    cy.get("#registerModal").should("be.visible");
   });
 });
